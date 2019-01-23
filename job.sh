e28dd97201e0c4a9ed08268cf1e7872d4dcd36b0
Florent Denef <florent.denef@gmail.com>
2019-01-23 20:24:08 +0100

    changes.

--- job.sh
+++ job.sh
@@ -10,10 +10,10 @@
 
 #SBATCH --mem 50G
 #SBATCH -n 28
+##SBATCH -c 28
 #SBATCH -N 1
 
 #SBATCH --mail-user=<florent.denef@etudiant.univ-reims.fr>
 #SBATCH	--mail-type=ALL,TIME_LIMIT_50,TIME_LIMIT_80
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 srun ./prog totot 2
